b931ecd992d739b4e1fdcad9b27e4a788659e700
Денис Андросов <you@example.com>
2024-12-20 18:17:03 +0300

    update loading component

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,12 +1,13 @@
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { INote } from '../../interface/notes.interface';
-import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { LoadingTodoItemsComponent } from "../../share/loading-todo-items/loading-todo-items.component";
 
 @Component({
   selector: 'app-todo-item',
-  imports: [MatTooltipModule, CommonModule, CdkAccordionModule],
+  imports: [MatTooltipModule, CommonModule, CdkAccordionModule, LoadingTodoItemsComponent],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss',
 })
---
+++ src/app/share/loading-todo-items/loading-todo-items.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-todo-items',
+  imports: [CommonModule],
+  templateUrl: './loading-todo-items.component.html',
+  styleUrl: './loading-todo-items.component.scss',
+})
+export class LoadingTodoItemsComponent {}
