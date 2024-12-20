9c355c263412962c233c7519b9950c12be738ac3
Денис Андросов <you@example.com>
2024-12-20 17:35:47 +0300

    update button-description(add cdk-accordion)

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { INote } from '../../interface/notes.interface';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [MatTooltipModule, CommonModule],
+  imports: [MatTooltipModule, CommonModule, CdkAccordionModule],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss',
 })
@@ -17,7 +18,6 @@ export class TodoItemComponent {
   @Output() EditEvent = new EventEmitter<number>();
   @Output() DescriptionEvent = new EventEmitter<number>();
 
-
   isLoading = true;
 
   ngOnInit(): void {
@@ -25,7 +25,6 @@ export class TodoItemComponent {
       this.isLoading = false;
     }, 500);
   }
-
 
   onDelete(index: number) {
     this.DeleteEvent.emit(index);
